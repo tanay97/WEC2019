70dc235edf99fa0751118585a9cebde43d786bd1
JerryXie98 <jerry_xie@hotmail.com>
2019-07-19 18:27:35 -0400

    Adding python backend.

---
+++ server.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder="build/static", template_folder="build")
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=3000)
