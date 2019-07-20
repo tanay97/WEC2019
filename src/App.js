03f271267fdae0b36397bc78f9a8b5df017bce6f
JerryXie98 <jerry_xie@hotmail.com>
2019-07-19 21:51:23 -0400

    Seperating pages.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
 import React from 'react';
 import './App.css';
 
+import { Button } from 'react-bootstrap';
+
 import Generator from './components/Generator'
 import Bracket from './components/Bracket'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Tournament Bracket Generator</p>
-        {/* <Generator></Generator> */}
+class App extends React.Component {
+
+  state = {
+    submitted: false,
+  }
+
+  getBracket() {
+    this.setState({ submitted: true })
+  }
+
+  main() {
+    if (this.state.submitted) {
+      return (
         <Bracket></Bracket>
-      </header>
-    </div>
-  );
+      )
+    } else {
+      return (
+        <div>
+          <p>Generate Your Tournament!</p>
+          <Generator></Generator>
+          <br></br>
+          <Button variant="light" onClick={() => this.getBracket()}>Submit</Button>
+        </div>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.main()}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Bracket.jsx
+++ src/components/Bracket.jsx
@@ -1,25 +1,67 @@
 import React from 'react'
+import '../styles/Bracket.css'
 
-import { button } from 'react-bootstrap';
+import { InputGroup, FormControl, Button } from 'react-bootstrap'
 
 class Bracket extends React.Component {
 
 	state = {
 		matches: [
 			{
+				round: 1,
 				winner: '',
 				home: 'A',
 				away: 'B',
+				homeScore: '',
+				awayScore: '',
 			},
 			{
+				round: 1,
 				winner: '',
 				home: 'C',
 				away: 'D',
+				homeScore: '',
+				awayScore: '',
 			},
 			{
+				round: 1,
+				winner: '',
+				home: 'E',
+				away: 'F',
+				homeScore: '',
+				awayScore: '',
+			},
+			{
+				round: 1,
+				winner: '',
+				home: 'G',
+				away: 'H',
+				homeScore: '',
+				awayScore: '',
+			},
+			{
+				round: 2,
 				winner: '',
 				home: 0,
 				away: 1,
+				homeScore: '',
+				awayScore: '',
+			},
+			{
+				round: 2,
+				winner: '',
+				home: 2,
+				away: 3,
+				homeScore: '',
+				awayScore: '',
+			},
+			{
+				round: 3,
+				winner: '',
+				home: 4,
+				away: 5,
+				homeScore: '',
+				awayScore: '',
 			}
 		]
 	}
@@ -59,21 +101,59 @@ class Bracket extends React.Component {
 		this.setState({ [e.target.id]: e.target.value });
 	}
 
+	selectWinner(m, i) {
+		if (m.home !== parseInt(m.home, 10) && m.away !== parseInt(m.away, 10)) {
+			return (
+				// <div>
+				// 	<InputGroup className="mb-1">
+				// 		<InputGroup.Prepend>
+				// 			<InputGroup.Text>{m.home}</InputGroup.Text>
+				// 		</InputGroup.Prepend>
+				// 		<FormControl className='score'
+				// 			placeholder={m.home + '\'s score e.g. 50'}
+				// 		/>
+				// 		<InputGroup.Prepend>
+				// 			<InputGroup.Text>{m.away}</InputGroup.Text>
+				// 		</InputGroup.Prepend>
+				// 		<FormControl
+				// 			placeholder={m.away + '\'s score e.g. 50'}
+				// 		/>
+				// 	</InputGroup>
+				// 	<Button variant="light" onClick={this.updateWinner()}>Submit Score</Button>
+				// </div>
+				
+				<div>
+					<select class="form-control" id="type" onChange={(e) => this.updateWinner(e, i)}>
+						<option>Select Winner</option>
+						<option>{m.home}</option>
+						<option>{m.away}</option>
+					</select>
+				</div>
+			)
+		}
+	}
+
+	presentWinner() {
+		if (this.state.matches[this.state.matches.length-1].winner !== '') {
+			return (
+				<h2>{'Congratulations the winner is ' + this.state.matches[this.state.matches.length-1].winner}</h2>
+			)
+		}
+	}
+
 	render() {
 		return(
 			<div>
 				{this.state.matches.map((m, i) => {
 					return(
-						<div>
-							<p>{m.home + ' vs ' + m.away}</p>
-							<select class="form-control" id="type" onChange={(e) => this.updateWinner(e, i)}>
-								<option>Select Winner</option>
-								<option>{m.home}</option>
-								<option>{m.away}</option>
-							</select>
+						<div key={i}>
+							<p>{'Round ' + m.round + ': ' + m.home + ' vs ' + m.away}</p>
+							{this.selectWinner(m, i)}
+							<br></br>
 						</div>
 					)
 				})}
+				{this.presentWinner()}
 			</div>
 		)
 	}
