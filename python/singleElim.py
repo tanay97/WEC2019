ce682c046ef845824348b87a435c8a0d3d179574
Ishan Kapoor <ikapoor@uwaterloo.ca>
2019-07-19 21:03:16 -0400

    returning out_match

--- python/singleElim.py
+++ python/singleElim.py
@@ -23,6 +23,7 @@ def single_elimination(teams):
 
     valid_teams = teams
     match_index = 1
+    round = 1
     while (len(valid_teams) > 1):
         n = len(valid_teams)
         num_byes = 0 if n % 2 == 0 else 1
@@ -34,7 +35,7 @@ def single_elimination(teams):
 
         for i in range(int(len(players)/2)):
             matches[match_index] = [ players[i], players[len(players) -i -1]]
-            out_match[match_index] = {"home" :  players[i],  "away" : players[len(players) -i -1]}
+            out_match[match_index] = {"home" :  players[i],  "away" : players[len(players) -i -1], "round": round}
             #print("Match " + str(match_index) + " : " + players[i] + " Vs. " + players[len(players) -i -1])
             match_index = match_index + 1
 
@@ -42,9 +43,10 @@ def single_elimination(teams):
             print(bye + " gets a bye")
         valid_teams = byes
         for match in matches.keys():
-            valid_teams.append(match )
-
-        print(out_match)
+            valid_teams.append(match)
+        round  = round +1
+    print(out_match)
+    return out_match
 
 
 
