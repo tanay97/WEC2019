491d649a16e7c47d4028414513883c8a4c4cb089
Ishan Kapoor <ikapoor@uwaterloo.ca>
2019-07-19 23:28:48 -0400

    double

--- python/doubleElim.py
+++ python/doubleElim.py
@@ -6,15 +6,27 @@ def double_elimination(teams):
 
     round = 1;
     valid_teams = teams
+
     n = len(valid_teams)
     idx = 0 if n%2 == 0 else 1
+    byes = valid_teams[0:idx]
     valid_teams = teams[idx:]
     n = len(valid_teams)
     for i in range(int(len(valid_teams)/2)):
-        matches.append({"home" : valid_teams[i], "away" : valid_teams[n -i -1], "round" : round})
+        matches.append({"home" : valid_teams[i], "away" : valid_teams[n -i -1], "round" : round, "winner" : ""})
 
 
-    print (matches)
+    winners = byes
+    losers = []
+    for i in range(len(matches)):
+        winners.append("winner_" + str(i))
+        losers.append("loser_" + str(i))
+
+    for k in single_elimination(winners, len(matches) ,2):
+        matches.append(k)
+    for k in single_elimination(losers, len(matches) , 2):
+        matches.append(k)
+    print(matches)
 
 
 
@@ -22,6 +34,20 @@ def double_elimination(teams):
 
 
 
-double_elimination(["a", "b" , "c" , "d" ,"e", "f", "g"])
+    #
+    # print (matches)
+    # print('\n')
+    # print(winners)
+    # print('\n')
+    # print(losers)
+    # print('\n')
+
+
+
+
+
+
+
+double_elimination(["a", "b" , "c" , "d", 'e', 'f' ,'g','h','i'])
 
 
--- python/singleElim.py
+++ python/singleElim.py
@@ -1,29 +1,12 @@
 import math
 
-def print_first_round(valid_teams, matches):
-    n = len(valid_teams)
-    k = int(math.log2(n))
-    num_byes = 0 if n%2 == 0 else 1
-    match_index = 1
 
-    byes = valid_teams[0:num_byes]
-    players = valid_teams[num_byes:]
-
-
-    for i in range(int(len(players) / 2)):
-        matches[match_index] = [players[i], players[len(players) - i - 1]]
-        print("Match " + str(match_index) + " : " + players[i] + " Vs. " + players[len(players) - i - 1])
-        match_index = match_index + 1
-
-    for bye in byes:
-        print(bye + " gets a bye")
-
-def single_elimination(teams):
+def single_elimination(teams, starting_index = 0, round = 1):
     out_match = []
 
     valid_teams = teams
-    match_index = 0
-    round = 1
+    match_index = starting_index
+
     while (len(valid_teams) > 1):
         n = len(valid_teams)
         num_byes = 0 if n % 2 == 0 else 1
@@ -43,8 +26,38 @@ def single_elimination(teams):
         for match in matches.keys():
             valid_teams.append(match)
         round  = round +1
-    print(out_match)
+    #print(out_match)
     return out_match
+
+
+# def single_elimination(teams, starting_index):
+#     out_match = []
+#
+#     valid_teams = teams
+#     match_index = starting_index
+#     round = 1
+#     while (len(valid_teams) > 1):
+#         n = len(valid_teams)
+#         num_byes = 0 if n % 2 == 0 else 1
+#
+#         byes = valid_teams[0:num_byes]
+#         players = valid_teams[num_byes:]
+#         matches = {}
+#
+#         for i in range(int(len(players) / 2)):
+#             matches[match_index] = [players[i], players[len(players) - i - 1]]
+#             out_match.append({"home": players[i], "away": players[len(players) - i - 1], "round": round, "winner": ""})
+#             # print("Match " + str(match_index) + " : " + players[i] + " Vs. " + players[len(players) -i -1])
+#             match_index = match_index + 1
+#
+#         valid_teams = byes
+#         for match in matches.keys():
+#             valid_teams.append(match)
+#         round = round + 1
+#     # print(out_match)
+#     return out_match
+
+
 
 # def get_rankings(matches = {}):
 #     rounds = 0
@@ -59,7 +72,7 @@ def single_elimination(teams):
 
 
 
-single_elimination(["penis", "penis2", "penis3" , "penis4", "penis5"])
+#single_elimination(["penis", "penis2", "penis3" , "penis4", "penis5"])
 
 
 
